5bc7c62d5dbc012155196cabce3e39d0bb3de22d
lingya <lingya@517na.com>
2024-01-05 15:10:11 +0800

    feat: 1944. 队列中可以看到的人数
    看到的人数实际是出栈的人

---
+++ algorithm/src/main/java/letcode/october/_1944.java
@@ -0,0 +1,25 @@
+package letcode.october;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class _1944 {
+    public int[] canSeePersonsCount(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = heights.length;
+        int[] res = new int[n];
+        // 反着遍历
+        for(int i = n-1; i>=0; i--){
+            int h = heights[i];
+            while (!stack.isEmpty() && h > stack.peek()){
+                stack.pop();
+                res[i]++;
+            }
+            if(!stack.isEmpty()){
+                res[i]++;
+            }
+            stack.push(h);
+        }
+        return res;
+    }
+}
